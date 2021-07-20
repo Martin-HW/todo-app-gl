a7293a2006a2350e79d2a59fa66248a90cfebbe9
martin suarez <msuarez@webcreek.com>
2021-07-20 13:44:41 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [dataList, setDataList] = useState([]);
+    const [element, setElement] = useState({
+        name: '',
+        done: false,
+    });
+
+    const handleChange = e => {
+        const { name, value } = e.target;
+        setElement(prevState => ({
+            ...prevState,
+            [name]: value,
+        }));
+        console.log(e);
+    };
+
+    const createElement = () => {
+        console.log(element, 'elementttt');
+        console.log('createelemenet function');
+        setDataList(dataList.concat(element));
+        setElement({ name: '', donde: false });
+        console.log(dataList);
+    };
+
+    const styleDone = val => {
+        console.log(val.name);
+        setDataList(
+            dataList.filter(v => {
+                return v.name !== val.name;
+            })
+        );
+    };
+
+    return (
+        <div>
+          <h2>To do List</h2>
+            <input
+                type="text"
+                name="name"
+                value={element && element.name}
+                onChange={handleChange}
+            />
+            <button
+                onClick={() => {
+                    createElement();
+                }}
+            >
+                Click here
+            </button>
+            <ul>
+                {dataList.map((val, ind, arr) => {
+                    return (
+                        <li key={ind}>
+                            <i>{val.name}</i>
+                            {'  '}
+                            <button
+                                onClick={() => {
+                                    styleDone(val);
+                                }}
+                            >
+                                completed
+                            </button>
+                        </li>
+                    );
+                })}
+            </ul>
+        </div>
+    );
+};
 
 export default App;
